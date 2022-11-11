335f451046211a58e092946da45ade1824232012
bath0ry <plylegacy@gmail.com>
2022-11-10 21:56:47 -0300

    add image network api

--- lib/dog_service.dart
+++ lib/dog_service.dart
@@ -1,8 +1,17 @@
 import 'package:dio/dio.dart';
+import 'package:dogs/dog_model.dart';
 
 class DogService {
   DogService(this.dio);
   final Dio dio;
 
-  
+  Future<DogModel> getDog() async {
+    const url = 'https://dog.ceo/api/breeds/image/random';
+    try {
+      final response = await dio.get(url);
+      return DogModel.fromJson(response.data);
+    } catch (e) {
+      throw Exception('Erro ao pegar dogs');
+    }
+  }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,13 +1,48 @@
+import 'package:dio/dio.dart';
+import 'package:dogs/dog_model.dart';
+import 'package:dogs/dog_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final DogService service;
+  @override
+  void initState() {
+    service = DogService(Dio());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.network(
-          'https://images.dog.ceo/breeds/rottweiler/n02106550_148.jpg'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+      ),
+      body: FutureBuilder(
+          initialData: DogModel(message: ''),
+          future: service.getDog(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text("Erro"),
+              );
+            } else if (snapshot.connectionState == ConnectionState.done) {
+              DogModel data = snapshot.data as DogModel;
+              return Center(child: Image.network(data.message));
+            } else {
+              return Container();
+            }
+          }),
     );
   }
 }
